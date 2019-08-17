1580f66d7a4226d072ad435907a09c50b37474a8
Sebastian K <s3bk@protonmail.com>
2019-08-17 23:43:14 +0300

    add build stuff

--- index.js
+++ index.js
@@ -5,35 +5,47 @@ const rust = import('./pkg/font_wasm.js')
         M = module;
         return module.default();
     }).then(function() {
-        let font = new M.FontRef(new Uint8Array(data));
+        load_font_from_url("Roboto-Medium.ttf");
     });
 
-let FONTS = [];
+let ENTRIES = [];
 let TEXT = "Hello World!";
-let VIEWS = [];
 
-function load_font(url) {
+function load_font_from_url(url) {
     fetch(url)
-    .then(r => r.arrayBuffer())
-    .then(function(data) {
-        let font = new M.FontRef(new Uint8Array(data));
-        FONTS.append(font);
-    })
+        .then(r => r.arrayBuffer())
+        .then(load_font, url);
+}
+
+function load_font(data, name) {
+    let font = new M.FontRef(new Uint8Array(data));
+    let div = document.createElement("div");
+    let canvas = draw_text(font, TEXT);
+    div.appendChild(canvas);
+    let entry = {
+        name: name,
+        font: font,
+        div: div
+    };
+    document.getElementById("views").append(div);
+    ENTRIES.push(entry);
 }
         
-function draw_text(font) {
-    let image = font.draw_text(200, TEXT);
+function draw_text(font, text) {
+    let image = font.draw_text(100, text);
     let canvas = image2canvas(image);
-    let view = document.createElement("div");
-    view.appendChild(canvas);
-    VIEWS.append(view);
-    
-    document.body.appendChild(view);
+    return canvas;
 }
-function update_text() {
-    
+
+function update_text(e) {
+    let text = e.target.value;
+    ENTRIES.forEach(function(entry) {
+        let canvas = draw_text(entry.font, text);
+        let div = entry.div;
+        div.replaceChild(canvas, div.firstChild);
+    });
 }
-    
+        
 function image2canvas(image) {
     let canvas = document.createElement("canvas");
     let width = canvas.width = image.width();
@@ -44,3 +56,25 @@ function image2canvas(image) {
     ctx.putImageData(img_data,0,0);
     return canvas;
 }
+function add_fonts(files) {
+    for (var i = 0; i < files.length; i++) {
+        let file = files[i];
+        let reader = new FileReader();
+        reader.onload = function() {
+            load_font(reader.result, file.name);
+        };
+        reader.readAsArrayBuffer(file);
+    }
+}
+function drop_handler(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    add_fonts(e.dataTransfer.files);
+}
+function dragover_handler(e) {
+    e.stopPropagation();
+    e.preventDefault();
+}
+
+document.addEventListener("drop", drop_handler, false);
+document.addEventListener("dragover", dragover_handler, false);
---
+++ pkg/font_wasm.js
@@ -0,0 +1,292 @@
+
+let wasm;
+
+let cachegetUint8Memory = null;
+function getUint8Memory() {
+    if (cachegetUint8Memory === null || cachegetUint8Memory.buffer !== wasm.memory.buffer) {
+        cachegetUint8Memory = new Uint8Array(wasm.memory.buffer);
+    }
+    return cachegetUint8Memory;
+}
+
+let WASM_VECTOR_LEN = 0;
+
+function passArray8ToWasm(arg) {
+    const ptr = wasm.__wbindgen_malloc(arg.length * 1);
+    getUint8Memory().set(arg, ptr / 1);
+    WASM_VECTOR_LEN = arg.length;
+    return ptr;
+}
+
+let cachedTextEncoder = new TextEncoder('utf-8');
+
+let passStringToWasm;
+if (typeof cachedTextEncoder.encodeInto === 'function') {
+    passStringToWasm = function(arg) {
+
+
+        let size = arg.length;
+        let ptr = wasm.__wbindgen_malloc(size);
+        let offset = 0;
+        {
+            const mem = getUint8Memory();
+            for (; offset < arg.length; offset++) {
+                const code = arg.charCodeAt(offset);
+                if (code > 0x7F) break;
+                mem[ptr + offset] = code;
+            }
+        }
+
+        if (offset !== arg.length) {
+            arg = arg.slice(offset);
+            ptr = wasm.__wbindgen_realloc(ptr, size, size = offset + arg.length * 3);
+            const view = getUint8Memory().subarray(ptr + offset, ptr + size);
+            const ret = cachedTextEncoder.encodeInto(arg, view);
+
+            offset += ret.written;
+        }
+        WASM_VECTOR_LEN = offset;
+        return ptr;
+    };
+} else {
+    passStringToWasm = function(arg) {
+
+
+        let size = arg.length;
+        let ptr = wasm.__wbindgen_malloc(size);
+        let offset = 0;
+        {
+            const mem = getUint8Memory();
+            for (; offset < arg.length; offset++) {
+                const code = arg.charCodeAt(offset);
+                if (code > 0x7F) break;
+                mem[ptr + offset] = code;
+            }
+        }
+
+        if (offset !== arg.length) {
+            const buf = cachedTextEncoder.encode(arg.slice(offset));
+            ptr = wasm.__wbindgen_realloc(ptr, size, size = offset + buf.length);
+            getUint8Memory().set(buf, ptr + offset);
+            offset += buf.length;
+        }
+        WASM_VECTOR_LEN = offset;
+        return ptr;
+    };
+}
+/**
+*/
+export function main() {
+    wasm.main();
+}
+
+let cachedTextDecoder = new TextDecoder('utf-8');
+
+function getStringFromWasm(ptr, len) {
+    return cachedTextDecoder.decode(getUint8Memory().subarray(ptr, ptr + len));
+}
+
+const heap = new Array(32);
+
+heap.fill(undefined);
+
+heap.push(undefined, null, true, false);
+
+let heap_next = heap.length;
+
+function addHeapObject(obj) {
+    if (heap_next === heap.length) heap.push(heap.length + 1);
+    const idx = heap_next;
+    heap_next = heap[idx];
+
+    heap[idx] = obj;
+    return idx;
+}
+
+function getObject(idx) { return heap[idx]; }
+
+function dropObject(idx) {
+    if (idx < 36) return;
+    heap[idx] = heap_next;
+    heap_next = idx;
+}
+
+function takeObject(idx) {
+    const ret = getObject(idx);
+    dropObject(idx);
+    return ret;
+}
+
+function getArrayU8FromWasm(ptr, len) {
+    return getUint8Memory().subarray(ptr / 1, ptr / 1 + len);
+}
+/**
+*/
+export class FontRef {
+
+    static __wrap(ptr) {
+        const obj = Object.create(FontRef.prototype);
+        obj.ptr = ptr;
+
+        return obj;
+    }
+
+    free() {
+        const ptr = this.ptr;
+        this.ptr = 0;
+
+        wasm.__wbg_fontref_free(ptr);
+    }
+    /**
+    * @param {Uint8Array} data
+    * @returns {FontRef}
+    */
+    constructor(data) {
+        const ret = wasm.fontref_load(passArray8ToWasm(data), WASM_VECTOR_LEN);
+        return FontRef.__wrap(ret);
+    }
+    /**
+    * @param {number} font_size
+    * @param {string} text
+    * @returns {Image}
+    */
+    draw_text(font_size, text) {
+        const ret = wasm.fontref_draw_text(this.ptr, font_size, passStringToWasm(text), WASM_VECTOR_LEN);
+        return Image.__wrap(ret);
+    }
+}
+/**
+*/
+export class Image {
+
+    static __wrap(ptr) {
+        const obj = Object.create(Image.prototype);
+        obj.ptr = ptr;
+
+        return obj;
+    }
+
+    free() {
+        const ptr = this.ptr;
+        this.ptr = 0;
+
+        wasm.__wbg_image_free(ptr);
+    }
+    /**
+    * @returns {number}
+    */
+    width() {
+        const ret = wasm.image_width(this.ptr);
+        return ret >>> 0;
+    }
+    /**
+    * @returns {number}
+    */
+    height() {
+        const ret = wasm.image_height(this.ptr);
+        return ret >>> 0;
+    }
+    /**
+    * @param {Uint8Array} buffer
+    */
+    get_data(buffer) {
+        const ptr0 = passArray8ToWasm(buffer);
+        const len0 = WASM_VECTOR_LEN;
+        try {
+            wasm.image_get_data(this.ptr, ptr0, len0);
+        } finally {
+            buffer.set(getUint8Memory().subarray(ptr0 / 1, ptr0 / 1 + len0));
+            wasm.__wbindgen_free(ptr0, len0 * 1);
+        }
+    }
+}
+
+function init(module) {
+    if (typeof module === 'undefined') {
+        module = import.meta.url.replace(/\.js$/, '_bg.wasm');
+    }
+    let result;
+    const imports = {};
+    imports.wbg = {};
+    imports.wbg.__wbg_new_9e5262d219d2ab81 = function(arg0, arg1) {
+        const ret = new Function(getStringFromWasm(arg0, arg1));
+        return addHeapObject(ret);
+    };
+    imports.wbg.__wbg_call_9d76dec92c7e03fd = function(arg0, arg1) {
+        const ret = getObject(arg0).call(getObject(arg1));
+        return addHeapObject(ret);
+    };
+    imports.wbg.__wbindgen_object_drop_ref = function(arg0) {
+        takeObject(arg0);
+    };
+    imports.wbg.__wbindgen_jsval_eq = function(arg0, arg1) {
+        const ret = getObject(arg0) === getObject(arg1);
+        return ret;
+    };
+    imports.wbg.__wbg_self_be54a55a10c6aca2 = function(arg0) {
+        const ret = getObject(arg0).self;
+        return addHeapObject(ret);
+    };
+    imports.wbg.__wbg_crypto_f6e4a8333f296b80 = function(arg0) {
+        const ret = getObject(arg0).crypto;
+        return addHeapObject(ret);
+    };
+    imports.wbg.__wbindgen_is_undefined = function(arg0) {
+        const ret = getObject(arg0) === undefined;
+        return ret;
+    };
+    imports.wbg.__wbg_getRandomValues_565baea101aa0f78 = function(arg0) {
+        const ret = getObject(arg0).getRandomValues;
+        return addHeapObject(ret);
+    };
+    imports.wbg.__wbg_require_4552fe1e0abcb7e5 = function(arg0, arg1) {
+        const ret = require(getStringFromWasm(arg0, arg1));
+        return addHeapObject(ret);
+    };
+    imports.wbg.__wbg_getRandomValues_d12e9fc437d4f537 = function(arg0, arg1, arg2) {
+        getObject(arg0).getRandomValues(getArrayU8FromWasm(arg1, arg2));
+    };
+    imports.wbg.__wbg_randomFillSync_39ff05f18e54e0fc = function(arg0, arg1, arg2) {
+        getObject(arg0).randomFillSync(getArrayU8FromWasm(arg1, arg2));
+    };
+    imports.wbg.__wbindgen_throw = function(arg0, arg1) {
+        throw new Error(getStringFromWasm(arg0, arg1));
+    };
+
+    if ((typeof URL === 'function' && module instanceof URL) || typeof module === 'string' || (typeof Request === 'function' && module instanceof Request)) {
+
+        const response = fetch(module);
+        if (typeof WebAssembly.instantiateStreaming === 'function') {
+            result = WebAssembly.instantiateStreaming(response, imports)
+            .catch(e => {
+                console.warn("`WebAssembly.instantiateStreaming` failed. Assuming this is because your server does not serve wasm with `application/wasm` MIME type. Falling back to `WebAssembly.instantiate` which is slower. Original error:\n", e);
+                return response
+                .then(r => r.arrayBuffer())
+                .then(bytes => WebAssembly.instantiate(bytes, imports));
+            });
+        } else {
+            result = response
+            .then(r => r.arrayBuffer())
+            .then(bytes => WebAssembly.instantiate(bytes, imports));
+        }
+    } else {
+
+        result = WebAssembly.instantiate(module, imports)
+        .then(result => {
+            if (result instanceof WebAssembly.Instance) {
+                return { instance: result, module };
+            } else {
+                return result;
+            }
+        });
+    }
+    return result.then(({instance, module}) => {
+        wasm = instance.exports;
+        init.__wbindgen_wasm_module = module;
+        wasm.__wbindgen_start();
+        return wasm;
+    });
+}
+
+export default init;
+
